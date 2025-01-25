c7fd4515fef41193a2a455b5e060dce289af4344
rugs07 <rugwed07@gmail.com>
2025-01-25 16:09:50 +0530

    imagesz-index changed

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export default function Home() {
 
         <div className="flex justify-center space-x-4 mb-6">
           <button
-            className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded"
+            className="px-4 py-2 bg-blue-500 hover:bg-blue-600  z-10 text-white rounded cursor-pointer"
             onClick={() => handleStartGame("easy")}
           >
             Easy
